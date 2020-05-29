a16a2028e11544310c7b8e9a51e08e570f274132
James Mobus <mobusj@live.com>
2020-05-29 17:26:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ let dodger = document.getElementById("dodger");
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
-    let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10);
- 
-    dodger.style.left = `${left - 1}px`;
+    moveDodgerLeft();
   }
 });
 
