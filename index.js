a3ce9e96ddd55f7ec4c9ec335078c92d154defbd
James Mobus <mobusj@live.com>
2020-05-29 17:29:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.left.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(rightNumbers, 10);
  
   if (right > 0) {
     dodger.style.left = `${right + 1}px`;
